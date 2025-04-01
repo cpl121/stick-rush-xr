62c9cc1597674db9016adaa2ef6586c35c57eab0
cpl121 <cesar.peonlam@gmail.com>
2025-04-01 12:49:09 +0200

    @feat: add button to enter in VR WIP

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
+'use client';
+
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         STICK RUSH XR
+        <button
+            onClick={() => {alert('Entering VR...');}}
+            className="bg-[#000] text-[#fff] rounded-[8px] px-[16px] py-[8px] text-[16px] font-bold hover:bg-[#111] transition-colors duration-200 ease-in-out"
+        >
+            Enter VR
+        </button>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
