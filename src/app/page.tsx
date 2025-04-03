92ff6b734516a189296b195de32392d496af3fc0
cpl121 <cesar.peonlam@gmail.com>
2025-04-03 09:35:41 +0200

    feat: improve home styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,17 @@
 import Scene from '@/components/Scene';
 
 export default function Home() {
-
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 gap-16 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        STICK RUSH XR
-        <Scene />
+    <div className="flex flex-col h-screen">
+     <header className="py-4 bg-slate-700 text-white flex items-center justify-center">
+          <h1> STICK RUSH XR </h1>
+      </header>
+      <main className="flex-1 overflow-hidden">
+          <Scene />
       </main>
-      <footer className=" w-full row-start-3 flex items-center justify-center border-t border-[#eaeaea] pt-4 text-[#666]">
+      <footer className="w-full flex items-center justify-center border-t border-white py-4 text-gray-400">
         <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          className="hover:underline hover:underline-offset-4"
           href="https://github.com/cpl121"
           target="_blank"
           rel="noopener noreferrer"
