0c44cbe9f5df1d84c6dab08e09a2d3addba16776
cpl121 <cesar.peonlam@gmail.com>
2025-04-09 22:09:59 +0200

    feat: add sticks animation

--- src/components/Floor.tsx
+++ src/components/Floor.tsx
@@ -1,6 +1,13 @@
+import { usePlane } from '@react-three/cannon';
+
 const Floor = () => {
+  const [floorRef] = usePlane(() => ({
+    position: [0, -3, 0],
+    rotation: [-Math.PI / 2, 0, 0],
+  }));
+
   return (
-    <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -1, 0]}>
+    <mesh ref={floorRef} receiveShadow>
       <boxGeometry args={[5, 5, 0.5]} />
       <meshStandardMaterial color="#468585" emissive={'#468585'} />
     </mesh>
